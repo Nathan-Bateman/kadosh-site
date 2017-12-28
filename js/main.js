2531bb8a706e48b6b2f1aee294b8c832d9b779d1
Nathan-Bateman <nathan.bateman.jr@gmail.com>
2017-12-28 10:19:07 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var me = 'nathan';
+var me3 ='bateman';
+console.log(me + me3);
+$( document ).ready(function() {
+
+	});
+var kadoshViewModel = function () {
+
+
+
+	}
+
+ko.applyBindings(new kadoshViewModel());	
